fixed non-sp loss averaging - plots match now
Signed-off-by: Stas Bekman <stas.bekman@snowflake.com>

arctic_training/data/factory.py
@@ -24,7 +24,7 @@ import torch
 from datasets import concatenate_datasets
 from datasets import load_from_disk
 from torch.utils.data import DataLoader
-from torch.utils.data import RandomSampler
+from torch.utils.data import RandomSampler, DistributedSampler
 from transformers import PreTrainedTokenizerBase
 
 from arctic_training.callback.mixin import CallbackMixin
@@ -38,7 +38,7 @@ from arctic_training.registry import _validate_class_attribute_type
 from arctic_training.registry import _validate_class_method
 from arctic_training.debug import print_rank0, print_rank, exit
 from arctic_training.logging import logger
-from arctic_training.utils import local_main_process_first, is_local_main_process
+from arctic_training.utils import global_main_process_first, is_global_main_process
 
 if TYPE_CHECKING:
     from arctic_training.data.source import DataSource
@@ -83,11 +83,12 @@ class DataFactory(ABC, CallbackMixin, metaclass=RegistryMeta):
         """
         def get_data_split(split: str) -> Optional[DatasetType]:
 
-            # XXX: currently our data cache is local to each node, but to be generic this needs more work as we want:
+            # XXX: currently our data cache is shared between nodes, but to be generic this needs more work as we want:
             # - local_main_process_first if the cache is local per node
             # - global_main_process_first if the cache is on the shared fs (all nodes see it)
             # perhaps we could auto-detect if the cache path is on the shared fs vs local and do the right thing based on that?
-            with local_main_process_first():
+            with global_main_process_first():
+            #if 1:
                 data_sources = self._get_data_sources(split=split)
 
                 cache_path = self.cache_path(sources=data_sources, split=split)
@@ -113,9 +114,9 @@ class DataFactory(ABC, CallbackMixin, metaclass=RegistryMeta):
             #     print_rank(f'non-cached loading data dump {data}', skip=False)
             #exit()
 
-            # Must save the cache only once from rank 0 (local or global depending on the type of the fs cache resides on see the notes at the top of get_data_split) and only if it doesn't already exist
-            # XXX: has to match `with ...main_process_first` above should it change to global instead of local
-            if is_local_main_process() and self.config.use_data_cache and not cache_path.exists():
+            # Must save the cache only once from rank 0 (local or global depending on the type of the fs cache resides on (see the notes at the top of get_data_split) and only if it doesn't already exist
+            # XXX: has to match `with ...main_process_first` above should it change to local instead of global
+            if is_global_main_process() and self.config.use_data_cache and not cache_path.exists():
                 logger.info(f"Saving pre-processed data to cache path {cache_path.as_posix()}")
                 dataset.save_to_disk(cache_path.as_posix())
 
@@ -190,6 +191,9 @@ class DataFactory(ABC, CallbackMixin, metaclass=RegistryMeta):
         the dataset.
         """
         local_length = len(dataset)
+
+        # XXX: do we really need this? DistributedSampler already provides drop_last option
+
         if self.world_size > 1:
             data_length = torch.zeros(self.world_size).to(self.trainer.device)
             data_length[self.global_rank] = local_length
@@ -242,10 +246,16 @@ class DataFactory(ABC, CallbackMixin, metaclass=RegistryMeta):
     @callback_wrapper("create_dataloader")
     def create_dataloader(self, dataset: DatasetType) -> DataLoader:
         """Create a torch DataLoader from the dataset."""
+        sampler = DistributedSampler(dataset,
+                                     num_replicas=self.world_size,
+                                     rank=self.global_rank)
+
+
         return DataLoader(
             dataset,
             batch_size=self.micro_batch_size,
-            sampler=RandomSampler(dataset),
+            # sampler=RandomSampler(dataset),
+            sampler=sampler,
             num_workers=self.config.num_proc,
             drop_last=True,
         )

arctic_training/data/sft_factory.py
@@ -23,7 +23,7 @@ import numpy as np
 import torch
 from datasets import Dataset
 from torch.utils.data import DataLoader
-from torch.utils.data import RandomSampler
+from torch.utils.data import RandomSampler, DistributedSampler
 from tqdm import tqdm
 from transformers import BatchEncoding
 from transformers import PreTrainedTokenizerBase
@@ -149,14 +149,13 @@ class DataCollatorForCausalLM:
             for example in instances:
                 # print(f'{len(example["input_ids"])=}')
                 # print(f'{len(example["position_ids"])=}')
-                # XXX: should labels match as well?
                 if not (len(example["input_ids"]) == len(example["position_ids"]) == len(example["labels"])):
                     raise ValueError(f'got a borked batch {len(example["input_ids"])=} != {len(example["position_ids"])=} != {len(example["labels"])=} ')
                     #print(f'mismatch {len(example["input_ids"])=} != {len(example["position_ids"])=}')
                     #print(f'mismatch {example["input_ids"]=}')
                     #print(f'mismatch {example["position_ids"]=}')
-                else:
-                    print_rank0("example is ok", skip=False)
+                # else:
+                #     print_rank0("example is ok", skip=False)
 
             position_ids = [
                 torch.tensor(example["position_ids"]) for example in instances
@@ -171,9 +170,9 @@ class DataCollatorForCausalLM:
         labels = pad(labels, padding_value=IGNORE_INDEX)
         position_ids = pad(position_ids, padding_value=0, is_position_id=True)
 
-        print_rank(f"   {input_ids.shape=}", skip=False)
-        print_rank(f"{position_ids.shape=}", skip=False)
-        print_rank(f"      {labels.shape=}", skip=False)
+        # print_rank(f"   {input_ids.shape=}", skip=False)
+        # print_rank(f"{position_ids.shape=}", skip=False)
+        # print_rank(f"      {labels.shape=}", skip=False)
         # XXX: Added a similar check earlier - not sure if the earlier one is a better place
         if not (input_ids.shape == position_ids.shape == labels.shape):
             raise ValueError(f"{input_ids.shape=} != {position_ids.shape} != {labels.shape=} in DataLoader, can't continue")
@@ -402,11 +401,14 @@ class SFTDataFactory(DataFactory):
         return output
 
     def create_dataloader(self, dataset: DatasetType) -> DataLoader:
+        sampler = DistributedSampler(dataset,
+                                     num_replicas=self.world_size,
+                                     rank=self.global_rank)
         return DataLoader(
             dataset,
             collate_fn=DataCollatorForCausalLM(tokenizer=self.tokenizer),
             batch_size=self.micro_batch_size,
-            sampler=RandomSampler(dataset),
+            sampler=sampler,
             num_workers=self.config.num_proc,
             drop_last=True,
         )

arctic_training/trainer/sft_trainer.py
@@ -35,6 +35,52 @@ def to_device(batch: Dict, device: str) -> Dict:
         output[k] = v.to(device)
     return output
 
+import torch.nn as nn
+def fixed_cross_entropy(source, target, num_items_in_batch: int = None, ignore_index: int = -100, **kwargs):
+    reduction = "sum" if num_items_in_batch is not None else "mean"
+    loss = nn.functional.cross_entropy(source, target, ignore_index=ignore_index, reduction=reduction)
+    if reduction == "sum":
+        loss = loss / num_items_in_batch
+    return loss
+
+def ForCausalLMLossSharded(
+    logits, labels, vocab_size: int, num_items_in_batch: int = None, ignore_index: int = -100, shift_labels_pad_index: int = None, **kwargs
+):
+    # Upcast to float if we need to compute the loss to avoid potential precision issues
+    logits = logits.float()
+    labels = labels.to(logits.device)
+    # Shift so that tokens < n predict n
+    if shift_labels_pad_index is None:
+        shift_labels_pad_index = ignore_index
+    labels = nn.functional.pad(labels, (0, 1), value=shift_labels_pad_index)
+    shift_labels = labels[..., 1:].contiguous()
+
+    # Flatten the tokens
+    logits = logits.view(-1, vocab_size)
+    shift_labels = shift_labels.view(-1)
+    # Enable model parallelism
+    shift_labels = shift_labels.to(logits.device)
+    loss = fixed_cross_entropy(logits, shift_labels, num_items_in_batch, ignore_index, **kwargs)
+    return loss
+
+class FakeLoss(torch.autograd.Function):
+    """
+
+    """
+
+    @staticmethod
+    def forward(ctx, logits):
+        loss = logits.sum() * 0.0
+        ctx.logits_shape = logits.shape
+        ctx.device = logits.device
+        ctx.dtype = logits.dtype
+        return loss
+
+    @staticmethod
+    def backward(ctx, output_gracds):
+        logits_grads = torch.zeros(ctx.logits_shape, device=ctx.device, dtype=ctx.dtype)
+        return logits_grads, None, None
+
 
 class SFTTrainer(Trainer):
     name = "sft"
@@ -53,9 +99,29 @@ class SFTTrainer(Trainer):
             # XXX: weird
             batch["labels"] = batch["labels"].type(torch.LongTensor)
             outputs = self.model(**batch, use_cache=False)
+
+            logits = outputs.logits
+            print_rank(f"{logits=}", skip=False)
+            print_rank(f"logit nans: {torch.isnan(logits).sum()}", skip=False)
+            print_rank(f"logit infs: {torch.isinf(logits).sum()}", skip=False)
+
             loss = outputs.loss
+            self.model.backward(loss)
+
+            import torch.distributed.nn.functional
+            with torch.no_grad():
+                # average losses
+                losses_per_rank = torch.distributed.nn.functional.all_gather(loss)
+                print(f"LOSS {losses_per_rank=}")
+                average_loss = torch.cat([l.unsqueeze(0) for l in losses_per_rank], dim=0).mean()
+                print(f"LOSS {average_loss=}")
+
+            return average_loss
+
         else:
 
+            #print_rank(f"YYYY {batch['position_ids']=}", skip=False)
+            #exit()
             # ensure shapes are correct
             if not (batch["input_ids"].shape == batch["position_ids"].shape == batch["labels"].shape):
                 raise ValueError(f'Borked batch {batch["input_ids"].shape=} != {batch["position_ids"].shape=} != {batch["labels"].shape=}) in DataLoader->iter->next, cannot continue with Sequence parallelism')
@@ -80,6 +146,8 @@ class SFTTrainer(Trainer):
             # using approach (1) for now but might want to benchmark later the other 2 approaches
 
             see_memory_usage("before gathering", force=True)
+            #print_rank(f"{self.tokenizer.decode(batch['input_ids'][0])=}", skip=False)
+            #exit()
 
             # XXX: if using all_gather_object we can gather the whole batch at once and not per-key! so can drop the loop for that approach
             for k in batch.keys():
@@ -98,7 +166,10 @@ class SFTTrainer(Trainer):
                         micro_batches[rank][k] = tensor
                         print_rank(f"after gather: {rank} {k}: {micro_batches[rank][k].shape=}", skip=False)
                         #print_rank(f"after gather: {rank} {k}: {micro_batches[rank][k].device=}", skip=False)
-                        print_rank(f"after gather: {rank} {k}: {micro_batches[rank][k]=}", skip=False)
+                        #print_rank(f"after gather: {rank} {k}: {micro_batches[rank][k]=}", skip=False)
+                        if k == "input_ids":
+                            print_rank0(f"{self.tokenizer.decode(micro_batches[rank][k][0])=}", skip=False)
+
             #exit()
             loss_aggregate = 0
             # we need to chunk twice - each time on SP size level
@@ -108,6 +179,7 @@ class SFTTrainer(Trainer):
             see_memory_usage("after gathering", force=True)
             self.model.set_gradient_accumulation_boundary(False)
 
+            losses = []
             for sub_step_id in range(sp_world_size):
                 batch = micro_batches[sub_step_id]
 
@@ -127,20 +199,44 @@ class SFTTrainer(Trainer):
                 print_rank0(f"{sub_step_id=}: {seq_length=}")
                 print_rank0(f"{sub_step_id=}: {chunk_len=}")
 
-                for k in batch.keys():
-                    # we are not chunking labels!
-                    if k in ["input_ids", "position_ids"]:
-                        print_rank(f"SLICING {k} {chunk_len=}: {sp_rank=}", skip=False)
-                        batch[k] = batch[k][:, chunk_len*sp_rank:chunk_len*(sp_rank+1)].to(self.device)
+                # get the first label elem of each shard to be later used in the loss function
+                # the last rank will have -100 instead
+                shift_labels_pad_index = {}
+                for rank in range(sp_world_size):
+                    # XXX: careful - assuming bs=1 - need to generalize this for bs>1
+                    if rank+1 == sp_world_size:
+                        index = -100
                     else:
-                        print_rank(f"KEEPING {k} {batch[k].shape=}", skip=False)
-                        batch[k] = batch[k].to(self.device)
+                        index = batch["labels"][0][chunk_len*(rank+1)].item()
+                    shift_labels_pad_index[rank] = index
+                print_rank(f"{shift_labels_pad_index=}", skip=False)
+                #exit()
 
-                    #print_rank0(f"after sp: {k}: {batch[k].shape=}")
+                # to enable the correct mean calculation across shards before sharding the micro batch:
+                # 1. count the number of non- `-100`` elements per shard
+                # 2. and subtract one more element because of label shifting
+                non_skipped_items = {}
+                for rank in range(sp_world_size):
+                    non_skipped = (batch["labels"][:, chunk_len*rank:chunk_len*(rank+1)] != -100).sum().item()
+                    if non_skipped > 1:
+                        non_skipped -= 1
+                    non_skipped_items[rank] = non_skipped
+                print_rank(f"{non_skipped_items=}", skip=False)
+
+                # batch sharding
+                for k in batch.keys():
+                    print_rank(f"SLICING {k} {chunk_len=}: {sp_rank=}", skip=False)
+                    batch[k] = batch[k][:, chunk_len*sp_rank:chunk_len*(sp_rank+1)].to(self.device)
+                    # else:
+                    #     print_rank(f"KEEPING {k} {batch[k].shape=}", skip=False)
+                    #     batch[k] = batch[k].to(self.device)
+
+                    print_rank0(f"after sp: {k}: {batch[k].shape=}")
                     #print_rank0(f"after sp: {k}: {batch[k]=}")
                 #outputs = self.model(**batch, use_cache=False)
                 #loss = outputs.loss
                 see_memory_usage(f"{sub_step_id=} after chunking", force=True)
+
 
                 # XXX: this would be the same not just for SFT so probably should abstract it away
                 from deepspeed.utils import groups
@@ -150,50 +246,172 @@ class SFTTrainer(Trainer):
                 # because we have to gather logits from all sp ranks we have to do the loss function ourselves
                 # therefore remove labels to avoid an attempt to calculate loss by transformers
                 labels = batch.pop("labels")
-                #labels = labels.type(torch.LongTensor)
+                labels = labels.type(torch.LongTensor)
 
+                    #print_rank0(f"after sp: {k}: {batch[k].shape=}")
+                    #print_rank0(f"after sp: {k}: {batch[k]=}")
                 see_memory_usage(f"{sub_step_id=} before forward", force=True)
 
-                outputs = self.model(**batch, use_cache=False)
-                see_memory_usage(f"{sub_step_id=} after forward", force=True)
+                print_rank(f"{self.tokenizer.decode(batch['input_ids'][0])=}", skip=False)
 
+                outputs = self.model(**batch, use_cache=False)
                 logits = outputs.logits
-                #print_rank(f"{sub_step_id=}: {torch.norm(logits)=}")
+
+                see_memory_usage(f"{sub_step_id=} after forward", force=True)
+                print_rank(f"{labels=}", skip=False)
+                print_rank(f"{logits=}", skip=False)
+                print_rank(f"logit nans: {torch.isnan(logits).sum()}", skip=False)
+                print_rank(f"logit infs: {torch.isinf(logits).sum()}", skip=False)
+
+
+                if all((labels == -100).squeeze()):
+                    loss = (logits.sum() * 0.0).float()
+                    #loss = FakeLoss.apply(logits)
+                    # call a different loss that can handle -100's only
+                    #labels[0][2] = 0
+                else:
+                    loss = ForCausalLMLossSharded(logits=logits,
+                                                  labels=labels,
+                                                  shift_labels_pad_index=shift_labels_pad_index[sp_rank],
+                                                  vocab_size=self.model_unwrapped.config.vocab_size)
+
+                    # deal with the boundary issue which would lose one label element per rank
+                    #
+                    # In unsharded string we end up with (shift left):
+                    #
+                    # input_ids: [1 2 3 4 5 6 7    8   ]
+                    # labels   : [1 2 3 4 5 6 7    8   ]
+                    # shiftedl : [2 3 4 5 6 7 8 -100]
+                    #
+                    # when sharded we lose label 5 once shifted:
+                    #
+                    # input_ids: [1 2 3    4] [5 6 7    8]
+                    # labels   : [1 2 3    4] [5 6 7    8]
+                    # shiftedl : [2 3 4 -100] [6 7 8 -100]
+                    #
+                    # since HF doesn't let us pre-shift labels - we use this workaround:
+                    # - for all but the last shards: add another column of elements so that logits[-1] is random and labels[-1] is the first labels[0] of the next shard
+                    # - for the last shard keep things the same
+                    # so that it looks like:
+                    #
+                    # input_ids: [1 2 3 4    r] [5 6 7    8]
+                    # labels   : [1 2 3 4    5] [5 6 7    8]
+                    # shiftedl : [2 3 4 5 -100] [6 7 8 -100]
+                    #
+                    # where r = a random [bs, 1, vocab-size] element
+                    #
+                    # which is the same as our original unsharded sequence
+                    #
+                    # input_ids: [1 2 3 4 5 6 7    8]
+                    # labels:    [1 2 3 4 5 6 7    8]
+                    # shiftedl : [2 3 4 5 6 7 8 -100]
+                    # 
+                    # the other solution is to replace -100 padding with the first label of the next shard, so then we get:
+                    #
+                    # input_ids: [1 2 3 4]  [5 6 7 8]
+                    # labels   : [1 2 3 4]  [5 6 7 8]
+                    # shiftedl : [2 3 4 5]  [6 7 8 -100]
+
+                    # print_rank(f"b {labels.shape=}", skip=False)
+                    # print_rank(f"b {logits.shape=}", skip=False)
+                    # if sp_rank+1 != sp_world_size:
+                    #     # XXX: need to make shift_labels_pad_index work for bs>1 as it can be a different value for each bs entry
+                    #     #labels = torch.nn.functional.pad(labels, (0, 1), value=shift_labels_pad_index[sp_rank])
+                    #     labels = torch.cat((labels, torch.tensor(shift_labels_pad_index[sp_rank])[..., None, None]), dim=1)
+                    #     logits = torch.cat((logits, torch.zeros_like(logits[:,0,:])[:, None]), dim=1)
+
+                    # print_rank(f"a {labels.shape=}", skip=False)
+                    # print_rank(f"a {logits.shape=}", skip=False)
+
+                    # loss = self.model_unwrapped.loss_function(logits=logits, labels=labels, vocab_size=self.model_unwrapped.config.vocab_size)
+
+                #loss = outputs.loss
+                print_rank(f"LOSS local {loss=}", skip=False)
+
+                #exit()
+
+                # if torch.isnan(loss):
+                #     break
+                #     #continue
+                #     #loss = torch.tensor(0.0).to(self.device).requires_grad_() + 0.0
+                # free up temp mem (e.g. outputs.logits are huge)
+                #del outputs
+                # differentiable loss aggregation across ranks
+                import torch.distributed.nn.functional
+                #loss = torch.distributed.nn.functional.all_reduce(loss, op=torch.distributed.ReduceOp.SUM, group=sp_group)
+                losses_per_rank = torch.distributed.nn.functional.all_gather(loss, group=sp_group)
+                #print(f"LOSS {losses_per_rank=}")
+                print_rank(f"LOSS {losses_per_rank=}", skip=False)
+
+                # # filter out nans
+                # losses_per_rank = [l for l in losses_per_rank if not torch.isnan(l)]
+                # if len(losses_per_rank) == 0 :
+                #     # XXX: weird that all ranks get it sometimes even though labels are fine - bad data? skipping as a bad batch for now
+                #     continue
+
+
+                # since each shard may have a variable number of skipped elemented - need to calculate a weighted mean depending on each rank's contribution - this will also take care of loss=0 when all elements are -100 in a shard
+                # XXX: not expecting a total of 0-non-skipped items for div
+                loss = sum(losses_per_rank[rank] * non_skipped_items[rank] for rank in range(sp_world_size)) / sum(non_skipped_items.values())
+                # this is a much simpler version w/o weighting
+                # skip 0.0 entries when calculating total loss per batch
+                # loss = torch.stack(list(l for l in losses_per_rank if l != 0)).mean()
+
+                #loss = torch.cat([l.unsqueeze() for l in losses_per_rank], dim=0).mean()
+                #loss = sum(loss_per_rank) # / sp_world_size
+                #loss = sum(tensor_list)
+                print_rank(f"LOSS averaged {loss=}", skip=False)
+                print("LOSS", loss)
+                see_memory_usage(f"{sub_step_id=} after gathered loss", force=True)
+
+                #exit()
+
+                #logits = outputs.logits
+                print_rank(f"{sub_step_id=}: {torch.norm(logits)=}", skip=False)
                 #print_rank(f"{sub_step_id=}: {logits.shape=}")
                 #print_rank(f"{logits.dtype=}")
                 #print_rank(f"{sub_step_id=}: {labels.shape=}")
 
-                # XXX: stick into the trainer object
-                #sp_group = groups._get_sequence_parallel_group()
-                #sp_world_size = groups._get_sequence_parallel_world_size()
-                # we need the differentiable all_gather, which is the functional version of it
-                import torch.distributed.nn.functional
-                tensor_list = torch.distributed.nn.functional.all_gather(logits, sp_group)
-                # concatenate on the seqlen dimension
-                logits = torch.cat(tensor_list, dim=1)
-                del tensor_list
-                print_rank(f"after cat: {logits.shape=}")
-                see_memory_usage(f"{sub_step_id=} after cat", force=True)
+                # # XXX: stick into the trainer object
+                # #sp_group = groups._get_sequence_parallel_group()
+                # #sp_world_size = groups._get_sequence_parallel_world_size()
+                # # we need the differentiable all_gather, which is the functional version of it
+                # import torch.distributed.nn.functional
+                # tensor_list = torch.distributed.nn.functional.all_gather(logits, sp_group)
+                # # concatenate on the seqlen dimension
+                # logits = torch.cat(tensor_list, dim=1)
+                # del tensor_list
+                # print_rank(f"after cat: {logits.shape=}")
+                # see_memory_usage(f"{sub_step_id=} after cat", force=True)
 
                 #print_rank(f"LOSS {logits.shape=}: {labels.shape=}", skip=False)
 
-                loss = self.model_unwrapped.loss_function(logits=logits, labels=labels, vocab_size=self.model_unwrapped.config.vocab_size)
-                #print_rank0(f"intermediary {loss.item()*sp_world_size=}")
-                see_memory_usage(f"{sub_step_id=} after loss", force=True)
+                # loss = self.model_unwrapped.loss_function(logits=logits, labels=labels, vocab_size=self.model_unwrapped.config.vocab_size)
+                # #print_rank0(f"intermediary {loss.item()*sp_world_size=}")
 
-                # optimize memory
-                del logits
-                del labels
+                # # optimize memory
+                # del logits
+                # del labels
 
-                #loss = self.loss(batch)
-                loss_aggregate += loss.item()*sp_world_size
+                # #loss = self.loss(batch)
+                # loss_aggregate += loss.item()*sp_world_size
 
                 print_rank(f"{self.train_batch_idx}-{sub_step_id}: {loss.requires_grad=}")
                 print_rank(f"{self.train_batch_idx}-{sub_step_id}: {loss=}")
 
-                avg_loss = self.model.backward(loss)
-                print_rank0(f"zero loss: {avg_loss}")
+                self.model.backward(loss)
+                # print_rank(f"{labels[0][70:80]=}", skip=False)
+                # print_rank(f"{logits[0][70:80]=}", skip=False)
+                # print_rank(f'{batch["input_ids"][0][70:80]=}', skip=False)
+                # print_rank(f'{batch["input_ids"].grad[0][70:80]=}', skip=False)
+                # print_rank(f"{logits.grad[0][70:80]=}", skip=False)
+                # exit()
+
+                print_rank0(f"zero loss: {loss}", skip=False)
+                # print_rank0(f"zero loss: {avg_loss}", skip=False)
                 see_memory_usage(f"{sub_step_id=} after backward", force=True)
+
+                losses.append(loss.detach().item())
 
 
                 # from deepspeed.utils import safe_get_full_grad
@@ -208,6 +426,13 @@ class SFTTrainer(Trainer):
 
             self.model.set_gradient_accumulation_boundary(True)
 
+            # for per-iteration reporting
+            if len(losses) == 0:
+                loss = float('nan')
+            else:
+                loss = sum(losses) / len(losses)
+
+        #exit()
         # XXX: temp to measure the real memory usage
         # gc_empty_cuda_cache()
 

arctic_training/trainer/trainer.py
@@ -183,8 +183,8 @@ class UlyssesAttentionHF(torch.nn.Module):
                 local_head_count could be different for k,v vs q if it's not an MHA situation
             """
 
-            print_rank0('')
-            print_rank0(f"combine: before reshape:  {input.shape=}")
+            # print_rank0('')
+            # print_rank0(f"combine: before reshape:  {input.shape=}")
 
             # [sl_l bs hc hs] -> [sl_l bs ws hc_l hs]
             input = input.reshape([self.local_seq_length, \
@@ -193,18 +193,18 @@ class UlyssesAttentionHF(torch.nn.Module):
                                 local_head_count, \
                                 self.attn_head_size])
 
-            print_rank0(f"combine: after reshape:   {input.shape=}")
+            # print_rank0(f"combine: after reshape:   {input.shape=}")
 
             input = rearrange(input, 'sl_l bs ws hc_l hs -> ws sl_l bs hc_l hs').contiguous()
-            print_rank0(f"combine: after rearrange: {input.shape=}", skip=False)
+            # print_rank0(f"combine: after rearrange: {input.shape=}", skip=False)
 
             output = _DimZeroAllToAll.apply(self.process_group, input)
             #output = input
-            print_rank0(f"combine: after all2all:   {output.shape=}", skip=False)
+            # print_rank0(f"combine: after all2all:   {output.shape=}", skip=False)
 
             # [ws sl_l bs hc_l hs] -> [sl bs hc_l hs]
             output = output.reshape([self.global_seq_length, *output.shape[2:]]).contiguous()
-            print_rank0(f"combine: after reshape:   {output.shape=}")
+            # print_rank0(f"combine: after reshape:   {output.shape=}")
 
             # [sl bs hc_l hs]
             return output
@@ -218,7 +218,7 @@ class UlyssesAttentionHF(torch.nn.Module):
             returns output in shape: [sl_l bs em]
         """
 
-        print_rank0(f"partition: before reshape:  {input.shape=}")
+        # print_rank0(f"partition: before reshape:  {input.shape=}")
 
         # [sl bs em_l] -> [ws sl_l bs em_l]
         input = input.reshape([self.world_size, \
@@ -226,17 +226,17 @@ class UlyssesAttentionHF(torch.nn.Module):
                             self.batch_size, \
                             self.attn_head_size * self.attn_head_count // self.world_size]).contiguous()
 
-        print_rank0(f"partition: after reshape:   {input.shape=}", skip=False)
+        # print_rank0(f"partition: after reshape:   {input.shape=}", skip=False)
         output = _DimZeroAllToAll.apply(self.process_group, input)
         #output = input
-        print_rank0(f"partition: after all2all:   {output.shape=}", skip=False)
+        # print_rank0(f"partition: after all2all:   {output.shape=}", skip=False)
         output = rearrange(output, 'ws sl_l bs em_l -> sl_l bs ws em_l')
         #output = rearrange(output, 'ws sl_l bs ... -> sl_l bs ws ...')
-        print_rank0(f"partition: after rearrange: {output.shape=}")
+        # print_rank0(f"partition: after rearrange: {output.shape=}")
 
         # [sl_l bs ws em_l] -> [sl_l bs em]
         output = output.reshape([*output.shape[:2], -1]).contiguous()
-        print_rank0(f"partition: after reshape:   {output.shape=}")
+        # print_rank0(f"partition: after reshape:   {output.shape=}")
 
         # [sl_l bs em]
         return output
@@ -273,19 +273,19 @@ class UlyssesAttentionHF(torch.nn.Module):
             self.required_key_value_shape = torch.Size([self.local_seq_length] + list(self.required_key_value_shape)[1:])
             self.required_context_shape = torch.Size([self.global_seq_length] + list(self.required_context_shape)[1:])
 
-        print_rank0(f"forward 1 {query.shape=}")
-        print_rank0(f"forward 1 {key.shape=}")
-        print_rank0(f"forward 1 {value.shape=}")
+        # print_rank0(f"forward 1 {query.shape=}")
+        # print_rank0(f"forward 1 {key.shape=}")
+        # print_rank0(f"forward 1 {value.shape=}")
 
         query = rearrange(query, 'bs hc sl hs -> sl bs hc hs')
         key = rearrange(key,     'bs hc sl hs -> sl bs hc hs')
         value = rearrange(value, 'bs hc sl hs -> sl bs hc hs')
 
-        print_rank0(f"forward 2 {query.shape=}")
-        print_rank0(f"forward 2 {key.shape=}")
-        print_rank0(f"forward 2 {value.shape=}")
-        print_rank0(f"forward 2 {self.required_query_shape=}")
-        print_rank0(f"forward 2 {self.required_key_value_shape=}")
+        # print_rank0(f"forward 2 {query.shape=}")
+        # print_rank0(f"forward 2 {key.shape=}")
+        # print_rank0(f"forward 2 {value.shape=}")
+        # print_rank0(f"forward 2 {self.required_query_shape=}")
+        # print_rank0(f"forward 2 {self.required_key_value_shape=}")
 
         #print_rank0(f"{attention_mask.shape=}")
         # please don't remove the white-space vertical alignment in the error message
@@ -306,13 +306,13 @@ class UlyssesAttentionHF(torch.nn.Module):
 
         #query_layer = query_layer.reshape(query_layer.shape).contiguous()
 
-        print_rank0(f"{query_layer.shape=}")
-        print_rank0(f"{key_layer.shape=}")
-        print_rank0(f"{value_layer.shape=}")
+        # print_rank0(f"{query_layer.shape=}")
+        # print_rank0(f"{key_layer.shape=}")
+        # print_rank0(f"{value_layer.shape=}")
 
-        if attention_mask is not None:
-            print_rank0(f"{attention_mask.shape=}")
-            #print_rank0(f"{attention_mask=}")
+        # if attention_mask is not None:
+        #     print_rank0(f"{attention_mask.shape=}")
+        #     #print_rank0(f"{attention_mask=}")
 
         # XXX: stick into the trainer object
         from deepspeed.utils import groups
@@ -320,33 +320,33 @@ class UlyssesAttentionHF(torch.nn.Module):
         sp_world_size = groups._get_sequence_parallel_world_size()
 
         #exit()
-        debug_gathered_tensor(query_layer, sp_group, name="query_layer")
-        debug_gathered_tensor(key_layer, sp_group, name="key_layer")
-        debug_gathered_tensor(value_layer, sp_group, name="value_layer")
+        # debug_gathered_tensor(query_layer, sp_group, name="query_layer")
+        # debug_gathered_tensor(key_layer, sp_group, name="key_layer")
+        # debug_gathered_tensor(value_layer, sp_group, name="value_layer")
 
-        print_rank0(f"HF before real attn: {query_layer.shape=}")
-        print_rank0(f"HF before real attn: {key_layer.shape=}")
-        print_rank0(f"HF before real attn: {value_layer.shape=}")
-        print_rank0(f"HF before real attn: {torch.norm(query_layer)=}")
-        print_rank0(f"HF before real attn: {torch.norm(key_layer)=}")
-        print_rank0(f"HF before real attn: {torch.norm(value_layer)=}")
+        # print_rank0(f"HF before real attn: {query_layer.shape=}")
+        # print_rank0(f"HF before real attn: {key_layer.shape=}")
+        # print_rank0(f"HF before real attn: {value_layer.shape=}")
+        # print_rank0(f"HF before real attn: {torch.norm(query_layer)=}")
+        # print_rank0(f"HF before real attn: {torch.norm(key_layer)=}")
+        # print_rank0(f"HF before real attn: {torch.norm(value_layer)=}")
 
         # expects: [bs hc_l sl hs]
         context_layer, attn_weights = self.attn(module, query_layer, key_layer, value_layer, attention_mask, *args, **kwargs)
         # returns [bs sl hc_l hs]
 
-        debug_gathered_tensor(context_layer, sp_group, name="context_layer")
+        # debug_gathered_tensor(context_layer, sp_group, name="context_layer")
 
-        print_rank0(f"HF after real attn: {context_layer.shape=}")
-        print_rank0(f"HF after real attn: {torch.norm(context_layer)=}")
+        # print_rank0(f"HF after real attn: {context_layer.shape=}")
+        # print_rank0(f"HF after real attn: {torch.norm(context_layer)=}")
 
-        print_rank0(f"1 {context_layer.shape=}")
+        # print_rank0(f"1 {context_layer.shape=}")
         # [bs sl hc_l hs] -> [sl bs hc_l hs]'
         context_layer = rearrange(context_layer, 'bs sl ... -> sl bs ...')
-        print_rank0(f"2 {context_layer.shape=}")
+        # print_rank0(f"2 {context_layer.shape=}")
         context_layer = context_layer.reshape([*context_layer.shape[:2], -1])
-        print_rank0(f"3 {context_layer.shape=}")
-        print_rank0(f"{self.required_context_shape=}")
+        # print_rank0(f"3 {context_layer.shape=}")
+        # print_rank0(f"{self.required_context_shape=}")
 
         assert context_layer.shape == self.required_context_shape, \
                     f"The context shape {context_layer.shape} is not as expected shape {self.required_context_shape}"
@@ -355,16 +355,16 @@ class UlyssesAttentionHF(torch.nn.Module):
         output = self._partition_global_sequence(context_layer)
         # returns: [sl_l bs em]
 
-        print_rank0(f"1 {output.shape=}")
+        # print_rank0(f"1 {output.shape=}")
         output = rearrange(output, 'sl_l bs ... -> bs sl_l ...')
-        print_rank0(f"2 {output.shape=}")
+        # print_rank0(f"2 {output.shape=}")
 
         output = output.reshape([*output.shape[:2], -1])
-        print_rank0(f"3 {output.shape=}")
-        if attn_weights is not None:
-            print_rank0(f"{attn_weights.shape=}")
+        # print_rank0(f"3 {output.shape=}")
+        # if attn_weights is not None:
+        #     print_rank0(f"{attn_weights.shape=}")
 
-        debug_gathered_tensor(output, sp_group, name="output")
+        # debug_gathered_tensor(output, sp_group, name="output")
 
 
         # expects [bs sl em]
@@ -885,15 +885,23 @@ class Trainer(ABC, CallbackMixin, metaclass=RegistryMeta):
         #     avg_loss = self.model.backward(loss)
 
             loss = self.loss(batch)
-            self.model.backward(loss)
+            #self.model.backward(loss)
+
+
         else:
             # sp will do backward inside loss
             loss = self.loss(batch)
         see_memory_usage("after backward", force=True)
 
         self.model.step()
+
+        # import math
+        # if not math.isnan(loss):
+        #     print(f"{loss=}")
+        #     self.model.step()
+
         see_memory_usage("after step", force=True)
-        exit()
+        #exit()
 
             # # should loss be averaged over sp sub-steps and logged as such?
             # loss = loss_aggregate / sp_world_size
@@ -938,16 +946,26 @@ class Trainer(ABC, CallbackMixin, metaclass=RegistryMeta):
         #     avg_loss = self.model.backward(loss)
         #     print_rank0(f"zero loss: {avg_loss}")
 
-        #     from deepspeed.utils import safe_get_full_grad
-        #     print_rank0(f"{torch.norm(safe_get_full_grad(self.model.module.lm_head.weight))=}")
-        #     print_rank0(f"{torch.norm(safe_get_full_grad(self.model.module.model.layers[0].self_attn.q_proj.weight))=}")
+        from deepspeed.utils import safe_get_full_grad, safe_get_full_fp32_param
+        # print_rank0(f"!!! {torch.norm(safe_get_full_fp32_param(self.model.lm_head.weight))} lm_head.weight", skip=False)
+        # print_rank0(f"!!! {torch.norm(safe_get_full_fp32_param(self.model.model.layers[0].self_attn.q_proj.weight))} q.weight", skip=False)
 
-        #     self.model.step()
+        # # print_rank(f"end loss = {loss}")
+        # print_rank0(f"!!! {torch.norm(safe_get_full_grad(self.model.module.lm_head.weight))} lm_head.grad", skip=False)
+        # print_rank0(f"!!! {torch.norm(safe_get_full_grad(self.model.module.model.layers[0].self_attn.q_proj.weight))} q.grad", skip=False)
+        #exit()
 
-        # print_rank0(f"{torch.norm(self.model.lm_head.weight)=}")
-        # print_rank0(f"{torch.norm(self.model.model.layers[0].self_attn.q_proj.weight)=}")
-        # print_rank(f"end loss = {loss}")
-        # #exit()
+        for n, p in self.model.named_parameters():
+            print_rank(f"!!! {torch.norm(safe_get_full_fp32_param(p)):6.2f} {n}", skip=False)
+        for n, p in self.model.named_parameters():
+            print_rank(f"!!! {torch.norm(safe_get_full_grad(p)):6.2f} {n}", skip=False)
+        for n, p in self.model.named_parameters():
+            nans = torch.isnan(p).sum()
+            infs = torch.isinf(p).sum()
+            if nans > 0: print_rank(f"!!! GOT NANS {nans} {n}", skip=False)
+            if infs > 0: print_rank(f"!!! GOT INFS {infs} {n}", skip=False)
+
+        print(f"ITERATION {loss=}")
 
         # use deepspeed global step as golden truth
         self.global_step = self.model.global_steps
@@ -973,11 +991,18 @@ class Trainer(ABC, CallbackMixin, metaclass=RegistryMeta):
         self.train_batch_idx = 0
         for batch in self.train_batches:
             self.train_batch_idx += 1
-            print_rank(f"\n\n\n\n\nITERATION: {self.train_batch_idx}", skip=False)
+            print_rank(f"\n\n\n\n\nITERATION: {self.train_batch_idx} ", skip=False)
 
-            print_rank(f"before gather: : {batch['input_ids'].shape=}", skip=False)
-            print_rank(f"before gather: : {batch['labels'].shape=}", skip=False)
-            print_rank(f"before gather: : {batch['position_ids'].shape=}", skip=False)
+            # if self.train_batch_idx < 7:
+            #     continue
+            # if self.train_batch_idx == 10:
+            #     exit()
+
+            print_rank(f"{self.tokenizer.decode(batch['input_ids'][0])=}", skip=False)
+            # exit()
+
+            # print_rank(f"before gather: : {batch['labels'].shape=}", skip=False)
+            # print_rank(f"before gather: : {batch['position_ids'].shape=}", skip=False)
                 #print_rank0(f"before gather: {k}: {batch[k]=}")
             #exit()
             self.step(batch)