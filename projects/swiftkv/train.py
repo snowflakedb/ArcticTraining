8ee5a28e79e43901a6a3cdf48646f29cdd5d0708
Aurick Qiao <aurick.qiao@snowflake.com>
2025-04-14 16:42:03 +0000

    adapt swiftkv to new ulysses code

--- projects/swiftkv/train.py
+++ projects/swiftkv/train.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
 from typing import Any
 from typing import Union
 
@@ -26,6 +27,7 @@ from arctic_training import ModelConfig
 from arctic_training import SFTTrainer
 from arctic_training import TrainerConfig
 from arctic_training import logger
+from arctic_training.deepspeed import ChunkedMemEfficientLoss
 from arctic_training.trainer.sft_trainer import to_device
 from projects.swiftkv import llama_swiftkv
 from projects.swiftkv import qwen2_swiftkv
@@ -109,7 +111,103 @@ class SwiftKVTrainer(SFTTrainer):
     model_factory: SwiftKVModelFactory
     checkpoint_engine: Union[HFCheckpointEngine]
 
-    def loss(self, batch: Any) -> torch.Tensor:
+    def forward(self, batch):
+        batch = to_device(batch, self.device)
+
+        with torch.no_grad():
+            self.model.swiftkv(False)
+            self.model.eval()
+            teacher_outputs = self.model(**batch)
+
+        self.model.swiftkv(True)
+        self.model.train()
+        student_outputs = self.model(**batch)
+
+        return student_outputs, teacher_outputs
+
+    def loss(self, batch) -> torch.Tensor:
+        import torch
+        batch = to_device(batch, self.device)
+
+        if self.config.sequence_parallel_size == 1:
+            student_outputs, teacher_outputs = self.forward(batch)
+
+            loss = self.distillation_loss(
+                student_outputs.logits,
+                teacher_outputs.logits,
+                temperature=self.config.temperature,
+            )
+
+            logger.info(
+                f"student loss: {student_outputs.loss.item()}, teacher loss:"
+                f" {teacher_outputs.loss.item()}, distill loss: {loss.item()}"
+            )
+        else:
+            # Ulysses SP
+            # expectations:
+            # 1. batch has labels replaced with shift_labels (which are already preshifted)
+            # 2. this rank deals with a seqlen dimension shard so once the loss is calculated it needs to do a differentiable weighted loss average to get the grads right
+
+            if "labels" in batch:
+                raise ValueError(
+                    "found labels in batch - they shouldn't be there, instead shift_labels should be there - check"
+                    " that UlyssesAttentionHFDataLoaderWrapper has been applied to the original DataLoader object"
+                )
+            if "shift_labels" not in batch:
+                raise ValueError(
+                    "shift_labels are missing from the batch - check that UlyssesAttentionHFDataLoaderWrapper has been"
+                    " applied to the original DataLoader object"
+                )
+
+            shift_labels = batch.pop("shift_labels")
+            student_outputs, teacher_outputs = self.forward(batch)
+            logits = torch.cat([student_outputs.logits, teacher_outputs.logits], dim=-1)
+
+            # XXX: parameterize
+            num_loss_logit_shards: Any = "auto"
+
+            if all((shift_labels == -100).squeeze()):
+                # this is the case where all labels in a micro-batch are -100 (very common for SFT) - CE returns `nan` in this case, so we don't want to call loss and instead create a differentiable loss `0` which will also set all the grads to `0` in `backward` - the effect of this is akin to a perfect score where the model needs no adjustment since grads will be all zeros.
+                # XXX: should this be float and not the original dtype?
+                loss = (logits.sum() * 0.0).float()
+            else:
+                if num_loss_logit_shards == "auto":
+                    # parameterize to about 1GB fp32 logits shards
+                    slice_size_in_gb = 1  # XXX: make configurable?
+                    size_in_gb = logits.numel() * 4 / 2**30  # fp32
+                    # the sp shard's seqlen sp shard can be easily not divisible by the derived number of chunked loss shards, so we use the uppper ceiling and allow the last chunk to be shorter than the rest
+                    num_loss_logit_shards = math.ceil(size_in_gb / slice_size_in_gb)
+                    # print(f"derived {num_loss_logit_shards} shards for size {size_in_gb}GB")
+                if num_loss_logit_shards > 1:
+                    loss = ChunkedMemEfficientLoss.apply(
+                        self.distillation_loss_sp,
+                        logits,
+                        self.model_unwrapped.config.vocab_size,
+                        shift_labels,
+                        num_loss_logit_shards,
+                    )
+                else:
+                    # XXX: for some reason this was failing with zero1 w/ previous design - need to retest with the new design
+                    loss = self.distillation_loss_sp(
+                        logits=logits,
+                        labels=None,
+                        vocab_size=self.model_unwrapped.config.vocab_size,
+                        shift_labels=shift_labels,
+                    )
+
+            # differentiable weighted per-shard-loss aggregation across ranks
+            import torch.distributed.nn.functional
+
+            losses_per_rank = torch.distributed.nn.functional.all_gather(loss, group=self.sp_group)
+            good_tokens = sum((shift_labels != -100).view(-1))
+            good_tokens_per_rank = torch.distributed.nn.functional.all_gather(good_tokens, group=self.sp_group)
+            loss = sum(losses_per_rank[rank] * good_tokens_per_rank[rank] for rank in range(self.sp_world_size)) / sum(
+                good_tokens_per_rank
+            )
+
+        return loss
+
+    def loss_old(self, batch: Any) -> torch.Tensor:
         batch = to_device(batch, self.device)
 
         with torch.no_grad():
@@ -149,4 +247,13 @@ class SwiftKVTrainer(SFTTrainer):
                 dim=dim,
             )
             * temperature**2
+        )
+
+    def distillation_loss_sp(self, logits, labels=None, vocab_size=None, shift_labels=None):
+        student_logits, teacher_logits = torch.chunk(logits, 2, dim=-1)
+
+        return self.distillation_loss(
+            student_logits,
+            teacher_logits,
+            temperature=self.config.temperature,
         )
