e86f154c94c36e3f35d4bf5eae7c283804b0fa2f
Stas Bekman <stas.bekman@snowflake.com>
2025-05-13 18:23:24 +0000

    cleanup
    Signed-off-by: Stas Bekman <stas.bekman@snowflake.com>

--- arctic_training/model/liger_factory.py
+++ arctic_training/model/liger_factory.py
@@ -31,16 +31,16 @@ class LigerModelFactory(HFModelFactory):
             raise ImportError(
                 "You need to install the liger-kernel package to use LigerKernel models: `pip install liger-kernel`"
             )
-        # liger_version_min = "0.5.9"  # shift_labels support
-        # liger_version_have = importlib.metadata.version("liger_kernel")
-        # if version.parse(liger_version_have) < version.parse(liger_version_min):
-        #     raise ValueError(
-        #         f"liger-kernel>={liger_version_min} is required, but you have liger-kernel=={liger_version_have}"
-        #     )
+        liger_version_min = "0.5.9"  # shift_labels support
+        liger_version_have = importlib.metadata.version("liger_kernel")
+        if version.parse(liger_version_have) < version.parse(liger_version_min):
+            raise ValueError(
+                f"liger-kernel>={liger_version_min} is required, but you have liger-kernel=={liger_version_have}"
+            )
 
         # Disable liger's mlp override if we are using our mlp override
-        # XXX: it might be possible to combine the 2 in the future to benefit from the efficient liger swiglu kernel, but currently liger monkey patches the MLP class and thus we would have a race condition on who gets the override.
         swiglu = False if self.trainer.config.tiled_mlp_compute else True
+        # XXX: it might be possible to combine the 2 in the future to benefit from the efficient liger swiglu kernel, but currently liger monkey patches the MLP class and thus we would have a race condition on who gets the override.
 
         return AutoLigerKernelForCausalLM.from_pretrained(
             self.config.name_or_path,
