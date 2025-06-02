076e9b5d7560b25f2bd1aff6c3573c0279ba0874
Aurick Qiao <aurick.qiao@snowflake.com>
2025-06-02 19:05:06 -0400

    Add DeepseekV2SwiftKV and reorganize swiftkv project (#192)

--- projects/swiftkv/eval.py
+++ projects/swiftkv/eval.py
@@ -15,11 +15,8 @@
 
 from lm_eval.__main__ import cli_evaluate
 
-from projects.swiftkv import llama_swiftkv
-from projects.swiftkv import qwen2_swiftkv
-
-llama_swiftkv.register_auto()
-qwen2_swiftkv.register_auto()
+from projects.swiftkv.models import register_all_swiftkv
 
 if __name__ == "__main__":
+    register_all_swiftkv()
     cli_evaluate()
