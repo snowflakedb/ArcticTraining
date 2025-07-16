b64ea0816159e74cff9fbd9c979a28530b5c47b5
Pawel Renc <pawel.renc@snowflake.com>
2025-07-15 17:45:20 -0700

    Extract eval log iter condition (#237)
    Co-authored-by: Michael Wyatt <michael.wyatt@snowflake.com>

--- arctic_training/trainer/trainer.py
+++ arctic_training/trainer/trainer.py
@@ -316,9 +316,11 @@ class Trainer(ABC, CallbackMixin, metaclass=RegistryMeta):
             self.eval_dataloader,
             desc="Eval Batches",
             unit="batch",
-            disable=self.global_rank != 0
-            or (self.global_step // self.config.eval_interval % self.config.eval_log_iter_interval != 0),
+            disable=self.global_rank != 0 or not self.is_eval_log_iter(),
         )
+
+    def is_eval_log_iter(self) -> bool:
+        return self.global_step // self.config.eval_interval % self.config.eval_log_iter_interval == 0
 
     @cached_property
     def device(self) -> torch.device:
@@ -439,7 +441,7 @@ class Trainer(ABC, CallbackMixin, metaclass=RegistryMeta):
                 if self.config.eval_interval != 0 and self.global_step % self.config.eval_interval == 0:
                     self.evaluate()
 
-                    if not self.eval_batches.disable:
+                    if self.is_eval_log_iter():
                         self.metrics.print_summary(prefix="eval")
 
                         if self.wandb_experiment is not None:
