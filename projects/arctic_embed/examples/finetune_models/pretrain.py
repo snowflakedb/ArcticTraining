56592c129f8938c44e3a946e1cfb57ea086627a6
Qingcheng Zeng <qingcheng.zeng@snowflake.com>
2025-09-27 19:26:13 -0700

    update checkpoint path

--- projects/arctic_embed/examples/finetune_models/pretrain.py
+++ projects/arctic_embed/examples/finetune_models/pretrain.py
@@ -90,7 +90,7 @@ def build_trainer_config_from_json(cfg: dict) -> BiencoderTrainerConfig:
 
     cconf = BiencoderS3CheckpointConfig(
         output_dir=local_cache_dir,  # Used as staging directory
-        s3_path=cfg.get("S3_CHECKPOINT_PATH"),
+        s3_path=cfg.get("S3_CHECKPOINT_PATH", "s3://ml-dev-sfc-or-dev-misc1-k8s/cortexsearch/training/checkpoints/"),
         local_cache_dir=str(local_cache_dir),
         max_local_checkpoints=cfg.get("MAX_LOCAL_CHECKPOINTS", 3),
         save_every_n_steps=cfg.get("SAVE_STEPS", 300),
