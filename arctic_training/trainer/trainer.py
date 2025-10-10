7c5e1c1ba5d6cd96b9035b506f6fc93f6eb680eb
Stas Bekman <stas.bekman@snowflake.com>
2025-10-10 08:26:44 -0700

    do not log the first train iter to wandb
    Do not log the first train iteration to wandb, since it's a massive outlier on all performance metrics, which messes up the scale of the report.
    
    I'm not sure why this code has been dropped, so restoring it.

--- arctic_training/trainer/trainer.py
+++ arctic_training/trainer/trainer.py
@@ -456,8 +456,10 @@ class Trainer(ABC, CallbackMixin, metaclass=RegistryMeta):
                     metrics = {k: v for k, v in self.metrics.summary_dict.items()}
 
                     append_json_file(self.config.train_log_metrics_path, metrics)
-
-                    if self.wandb_experiment is not None:
+                    
+                    # do not log the first train iteration to wandb, since it's a massive outlier
+                    # on all performance metrics, which messes up the scale of the report
+                    if self.wandb_experiment is not None and self.global_step > 1:
                         metrics = {k: v for k, v in metrics.items() if k not in ["iter"]}
                         self.wandb_experiment.log(metrics, step=self.global_step)
 
