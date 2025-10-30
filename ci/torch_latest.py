774970a4b316ae96e842468e706963e6546705af
Stas Bekman <stas.bekman@snowflake.com>
2025-10-30 19:35:51 +0000

    wip
    Signed-off-by: Stas Bekman <stas.bekman@snowflake.com>

--- ci/torch_latest.py
+++ ci/torch_latest.py
@@ -39,8 +39,9 @@ image = (
     # .uv_pip_install_from_requirements(ROOT_PATH / "ci-requirements2.txt", gpu="any", extra_options="--no-build-isolation")
     # this installs the actual project
     # .run_commands("uv pip install --system .")
-    # .add_local_dir(ROOT_PATH, remote_path="/root/", copy=True)
-    # .run_commands("uv pip install --system /root")
+    # add_local_dir copies the repo over - since we use copy=True, it has to be run as late as possible to allow caching of the previous stages - so run it before installing the main repo itself.
+    .add_local_dir(ROOT_PATH, remote_path="/root/", copy=True)
+    .run_commands("uv pip install --system /root")
 )
 # fmt: on
 
@@ -67,16 +68,16 @@ def pytest():
     # overcome CI log buffering to see tests reported in real time
     os.environ["PYTHONUNBUFFERED"] = "1"
 
-    cmd = "uv pip install --system ."
-    print(f"Running: {cmd}")
-    subprocess.run(
-        cmd.split(),
-        check=True,
-        cwd=ROOT_PATH / ".",
-    )
+    # cmd = "uv pip install --system ."
+    # print(f"Running: {cmd}")
+    # subprocess.run(
+    #     cmd.split(),
+    #     check=True,
+    #     cwd=ROOT_PATH / ".",
+    # )
 
     # XXX: need to re-add `-n 4` when hardwired deepspeed dist init is removed from conftest.py - it conflicts with concurrent test runs as it assigns the same port to all tests
-    cmd = "pytest --disable-warnings --instafail -m gpu --verbose tests/trainer/test_ulysses_alst.py"
+    cmd = "pytest --disable-warnings --instafail -m gpu --verbose tests"
     # cmd = "pytest --disable-warnings --instafail -m gpu --verbose tests"
 
     print(f"Running: {cmd}")
