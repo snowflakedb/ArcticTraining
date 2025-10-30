2d780729c6be4afd296607f86a0f3496fde9d8e2
Stas Bekman <stas.bekman@snowflake.com>
2025-10-30 22:30:03 +0000

    wip
    Signed-off-by: Stas Bekman <stas.bekman@snowflake.com>

--- tests/trainer/test_ulysses_alst.py
+++ tests/trainer/test_ulysses_alst.py
@@ -35,7 +35,7 @@ model_name_or_path = "hf-internal-testing/tiny-random-LlamaForCausalLM"
 @require_torch_multi_gpu
 class TestTrainerWithLauncher(TestCasePlus):
     # def setUp(self):
-    #     super().setUp()111
+    #     super().setUp()1111
 
     @parameterized.expand(["flash_attention_2", "sdpa"])
     def test_ulysses_alst_e2e(self, attn_implementation):
