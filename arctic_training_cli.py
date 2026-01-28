2e5b0e7d30bd4e7dddeb90565494e7d4eb83d955
Thong Nguyen <thong.nguyen@snowflake.com>
2026-01-28 04:26:24 +0000

    minor fix

--- arctic_training_cli.py
+++ arctic_training_cli.py
@@ -25,9 +25,7 @@ def deepspeed_launch(config_file: str, mode: str, python_profile: str, deepspeed
     import deepspeed
     from deepspeed.launcher.runner import main as ds_runner
 
-    deepspeed.launcher.runner.EXPORT_ENVS = deepspeed.launcher.runner.EXPORT_ENVS + [
-        "WANDB",
-    ]
+    deepspeed.launcher.runner.EXPORT_ENVS = deepspeed.launcher.runner.EXPORT_ENVS + ["WANDB"]
 
     runner_name = "arctic_training_run"
     exe_path = shutil.which(runner_name)
